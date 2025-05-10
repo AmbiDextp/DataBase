370b4002677dcdea199dc92a2a8fd0ac746cb0f0
AmbiDextp <146813217+AmbiDextp@users.noreply.github.com>
2025-05-10 14:10:40 +1000

    final clearing

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ class CuratorCreate(BaseModel):
 class CuratorResponse(CuratorCreate):
     id: int
 
-@app.post("/curators", tags=["CRUD"], response_model=CuratorResponse)
+@app.post("/curators/", tags=["CRUD"], response_model=CuratorResponse)
 def create_curator(curator: CuratorCreate, db: dbDep):
     curator_db = Curator.model_validate(curator)
     db.add(curator_db)
@@ -127,18 +127,18 @@ def create_curator(curator: CuratorCreate, db: dbDep):
     db.refresh(curator_db)
     return curator_db
 
-@app.get("/curators", tags=["CRUD"], response_model=List[CuratorResponse])
+@app.get("/curators/", tags=["CRUD"], response_model=List[CuratorResponse])
 def read_curators(db: dbDep):
     return db.query(Curator).all()
 
-@app.get("/curators/{curator_id}", tags=["CRUD"])
+@app.get("/curators/{curator_id}/", tags=["CRUD"])
 def read_curator(curator_id: int, db: dbDep):
     curator = db.get(Curator, curator_id)
     if not curator:
         raise HTTPException(status_code=404, detail="Curator not found")
     return curator   
 
-@app.put("/curators/{curator_id}", tags=["CRUD"], response_model=CuratorResponse)
+@app.put("/curators/{curator_id}/", tags=["CRUD"], response_model=CuratorResponse)
 def update_curator(curator_id: int, curator: CuratorCreate, db: dbDep):
     curator_db = db.get(Curator, curator_id)
     if not curator_db:
@@ -149,7 +149,7 @@ def update_curator(curator_id: int, curator: CuratorCreate, db: dbDep):
     return curator_db
 
 
-@app.delete("/curators/{curator_id}", tags=["CRUD"])
+@app.delete("/curators/{curator_id}/", tags=["CRUD"])
 def delete_curator(curator_id: int, db: dbDep):
     curator = db.get(Curator, curator_id)
     if not curator:
@@ -166,7 +166,7 @@ class GroupCreate(BaseModel):
 class GroupResponse(GroupCreate):
     id: int
 
-@app.post("/groups", tags=["CRUD"], response_model=GroupResponse)
+@app.post("/groups/", tags=["CRUD"], response_model=GroupResponse)
 def create_group(group: GroupCreate, db: dbDep):
     group_db = Group.model_validate(group)
     db.add(group_db)
@@ -174,18 +174,18 @@ def create_group(group: GroupCreate, db: dbDep):
     db.refresh(group_db)
     return group_db
 
-@app.get("/groups", tags=["CRUD"], response_model=List[GroupResponse])
+@app.get("/groups/", tags=["CRUD"], response_model=List[GroupResponse])
 def read_groups(db: dbDep):
     return db.query(Group).all()
 
-@app.get("/groups/{group_id}", tags=["CRUD"])
+@app.get("/groups/{group_id}/", tags=["CRUD"])
 def read_group(group_id: int, db: dbDep):
     group = db.get(Group, group_id)
     if not group:
         raise HTTPException(status_code=404, detail="Group not found")
     return group   
 
-@app.put("/groups/{group_id}", tags=["CRUD"], response_model=GroupResponse)
+@app.put("/groups/{group_id}/", tags=["CRUD"], response_model=GroupResponse)
 def update_group(group_id: int, group: GroupCreate, db: dbDep):
     group_db = db.get(Group, group_id)
     if not group_db:
@@ -196,7 +196,7 @@ def update_group(group_id: int, group: GroupCreate, db: dbDep):
     db.refresh(group_db)
     return group_db
 
-@app.delete("/groups/{group_id}", tags=["CRUD"])
+@app.delete("/groups/{group_id}/", tags=["CRUD"])
 def delete_group(group_id: int, db: dbDep):
     group = db.get(Group, group_id)
     if not group:
@@ -214,7 +214,7 @@ class StudentCreate(BaseModel):
 class StudentResponse(StudentCreate):
     id: int
 
-@app.post("/students", tags=["CRUD"], response_model=StudentResponse)
+@app.post("/students/", tags=["CRUD"], response_model=StudentResponse)
 def create_student(student: StudentCreate, db: dbDep):
     student_db = Student.model_validate(student)
     db.add(student_db)
@@ -222,18 +222,18 @@ def create_student(student: StudentCreate, db: dbDep):
     db.refresh(student_db)
     return student_db
 
-@app.get("/students", tags=["CRUD"], response_model=List[StudentResponse])
+@app.get("/students/", tags=["CRUD"], response_model=List[StudentResponse])
 def read_students(db: dbDep):
     return db.query(Student).all()
 
-@app.get("/students/{student_id}", tags=["CRUD"])
+@app.get("/students/{student_id}/", tags=["CRUD"])
 def read_student(student_id: int, db: dbDep):
     student = db.get(Student, student_id)
     if not student:
         raise HTTPException(status_code=404, detail="Student not found")
     return student  
 
-@app.put("/students/{student_id}", tags=["CRUD"], response_model=StudentResponse)
+@app.put("/students/{student_id}/", tags=["CRUD"], response_model=StudentResponse)
 def update_student(student_id: int, student: StudentCreate, db: dbDep):
     student_db = db.get(Student, student_id)
     if not student_db:
@@ -245,7 +245,7 @@ def update_student(student_id: int, student: StudentCreate, db: dbDep):
     db.refresh(student_db)
     return student_db
 
-@app.delete("/students/{student_id}", tags=["CRUD"])
+@app.delete("/students/{student_id}/", tags=["CRUD"])
 def delete_student(student_id: int, db: dbDep):
     student = db.get(Student, student_id)
     if not student:
@@ -261,7 +261,7 @@ class CourseCreate(BaseModel):
 class CourseResponse(CourseCreate):
     id: int
 
-@app.post("/courses", tags=["CRUD"], response_model=CourseResponse)
+@app.post("/courses/", tags=["CRUD"], response_model=CourseResponse)
 def create_course(course: CourseCreate, db: dbDep):
     course_db = Course.model_validate(course)
     db.add(course_db)
@@ -269,18 +269,18 @@ def create_course(course: CourseCreate, db: dbDep):
     db.refresh(course_db)
     return course_db
 
-@app.get("/courses", tags=["CRUD"], response_model=List[CourseResponse])
+@app.get("/courses/", tags=["CRUD"], response_model=List[CourseResponse])
 def read_courses(db: dbDep):
     return db.query(Course).all()
 
-@app.get("/courses/{course_id}", tags=["CRUD"])
+@app.get("/courses/{course_id}/", tags=["CRUD"])
 def read_course(course_id: int, db: dbDep):
     course = db.get(Course, course_id)
     if not course:
         raise HTTPException(status_code=404, detail="Course not found")
     return course  
 
-@app.put("/courses/{course_id}", tags=["CRUD"], response_model=CourseResponse)
+@app.put("/courses/{course_id}/", tags=["CRUD"], response_model=CourseResponse)
 def update_course(course_id: int, course: CourseCreate, db: dbDep):
     course_db = db.get(Course, course_id)
     if not course_db:
@@ -290,7 +290,7 @@ def update_course(course_id: int, course: CourseCreate, db: dbDep):
     db.refresh(course_db)
     return course_db
 
-@app.delete("/courses/{course_id}", tags=["CRUD"])
+@app.delete("/courses/{course_id}/", tags=["CRUD"])
 def delete_course(course_id: int, db: dbDep):
     course = db.get(Course, course_id)
     if not course:
@@ -308,7 +308,7 @@ class MarkCreate(BaseModel):
 class MarkResponse(MarkCreate):
     id: int
 
-@app.post("/marks", tags=["CRUD"], response_model=MarkResponse)
+@app.post("/marks/", tags=["CRUD"], response_model=MarkResponse)
 def create_mark(mark: MarkCreate, db: dbDep):
     mark_db = Mark.model_validate(mark)
     db.add(mark_db)
@@ -316,18 +316,18 @@ def create_mark(mark: MarkCreate, db: dbDep):
     db.refresh(mark_db)
     return mark_db
 
-@app.get("/marks", tags=["CRUD"], response_model=List[MarkResponse])
+@app.get("/marks/", tags=["CRUD"], response_model=List[MarkResponse])
 def read_marks(db: dbDep):
     return db.query(Mark).all()
 
-@app.get("/marks/{mark_id}", tags=["CRUD"])
+@app.get("/marks/{mark_id}/", tags=["CRUD"])
 def read_mark(mark_id: int, db: dbDep):
     mark = db.get(Mark, mark_id)
     if not mark:
         raise HTTPException(status_code=404, detail="Mark not found")
     return mark  
 
-@app.put("/marks/{mark_id}", tags=["CRUD"], response_model=MarkResponse)
+@app.put("/marks/{mark_id}/", tags=["CRUD"], response_model=MarkResponse)
 def update_mark(mark_id: int, mark: MarkCreate, db: dbDep):
     mark_db = db.get(Mark, mark_id)
     if not mark_db:
@@ -339,7 +339,7 @@ def update_mark(mark_id: int, mark: MarkCreate, db: dbDep):
     db.refresh(mark_db)
     return mark_db
 
-@app.delete("/marks/{mark_id}", tags=["CRUD"])
+@app.delete("/marks/{mark_id}/", tags=["CRUD"])
 def delete_mark(mark_id: int, db: dbDep):
     mark = db.get(Mark, mark_id)
     if not mark:
@@ -355,7 +355,7 @@ class DegreeCreate(BaseModel):
 class DegreeResponse(DegreeCreate):
     id: int
 
-@app.post("/degrees", tags=["CRUD"], response_model=DegreeResponse)
+@app.post("/degrees/", tags=["CRUD"], response_model=DegreeResponse)
 def create_degree(degree: DegreeCreate, db: dbDep):
     degree_db = Degree.model_validate(degree)
     db.add(degree_db)
@@ -363,18 +363,18 @@ def create_degree(degree: DegreeCreate, db: dbDep):
     db.refresh(degree_db)
     return degree_db
 
-@app.get("/degrees", tags=["CRUD"], response_model=List[DegreeResponse])
+@app.get("/degrees/", tags=["CRUD"], response_model=List[DegreeResponse])
 def read_degrees(db: dbDep):
     return db.query(Degree).all()
 
-@app.get("/degrees/{degree_id}", tags=["CRUD"])
+@app.get("/degrees/{degree_id}/", tags=["CRUD"])
 def read_degree(degree_id: int, db: dbDep):
     degree = db.get(Degree, degree_id)
     if not degree:
         raise HTTPException(status_code=404, detail="Degree not found")
     return degree  
 
-@app.put("/degrees/{degree_id}", tags=["CRUD"], response_model=DegreeResponse)
+@app.put("/degrees/{degree_id}/", tags=["CRUD"], response_model=DegreeResponse)
 def update_degree(degree_id: int, degree: DegreeCreate, db: dbDep):
     degree_db = db.get(Degree, degree_id)
     if not degree_db:
@@ -384,7 +384,7 @@ def update_degree(degree_id: int, degree: DegreeCreate, db: dbDep):
     db.refresh(degree_db)
     return degree_db
 
-@app.delete("/degrees/{degree_id}", tags=["CRUD"])
+@app.delete("/degrees/{degree_id}/", tags=["CRUD"])
 def delete_degree(degree_id: int, db: dbDep):
     degree = db.get(Degree, degree_id)
     if not degree:
@@ -400,7 +400,7 @@ class PositionCreate(BaseModel):
 class PositionResponse(PositionCreate):
     id: int
 
-@app.post("/positions", tags=["CRUD"], response_model=PositionResponse)
+@app.post("/positions/", tags=["CRUD"], response_model=PositionResponse)
 def create_position(position: PositionCreate, db: dbDep):
     position_db = Position.model_validate(position)
     db.add(position_db)
@@ -408,18 +408,18 @@ def create_position(position: PositionCreate, db: dbDep):
     db.refresh(position_db)
     return position_db
 
-@app.get("/positions", tags=["CRUD"], response_model=List[PositionResponse])
+@app.get("/positions/", tags=["CRUD"], response_model=List[PositionResponse])
 def read_positions(db: dbDep):
     return db.query(Position).all()
 
-@app.get("/positions/{position_id}", tags=["CRUD"])
+@app.get("/positions/{position_id}/", tags=["CRUD"])
 def read_position(position_id: int, db: dbDep):
     position = db.get(Position, position_id)
     if not position:
         raise HTTPException(status_code=404, detail="Position not found")
     return position  
 
-@app.put("/positions/{position_id}", tags=["CRUD"], response_model=PositionResponse)
+@app.put("/positions/{position_id}/", tags=["CRUD"], response_model=PositionResponse)
 def update_position(position_id: int, position: PositionCreate, db: dbDep):
     position_db = db.get(Position, position_id)
     if not position_db:
@@ -429,7 +429,7 @@ def update_position(position_id: int, position: PositionCreate, db: dbDep):
     db.refresh(position_db)
     return position_db
 
-@app.delete("/positions/{position_id}", tags=["CRUD"])
+@app.delete("/positions/{position_id}/", tags=["CRUD"])
 def delete_position(position_id: int, db: dbDep):
     position = db.get(Position, position_id)
     if not position:
@@ -447,7 +447,7 @@ class TeacherCreate(BaseModel):
 class TeacherResponse(TeacherCreate):
     id: int
 
-@app.post("/teachers", tags=["CRUD"], response_model=TeacherResponse)
+@app.post("/teachers/", tags=["CRUD"], response_model=TeacherResponse)
 def create_teacher(teacher: PositionCreate, db: dbDep):
     teacher_db = Teacher.model_validate(teacher)
     db.add(teacher_db)
@@ -455,18 +455,18 @@ def create_teacher(teacher: PositionCreate, db: dbDep):
     db.refresh(teacher_db)
     return teacher_db
 
-@app.get("/teachers", tags=["CRUD"], response_model=List[TeacherResponse])
+@app.get("/teachers/", tags=["CRUD"], response_model=List[TeacherResponse])
 def read_teachers(db: dbDep):
     return db.query(Teacher).all()
 
-@app.get("/teachers/{teacher_id}", tags=["CRUD"])
+@app.get("/teachers/{teacher_id}/", tags=["CRUD"])
 def read_teacher(teacher_id: int, db: dbDep):
     teacher = db.get(Teacher, teacher_id)
     if not teacher:
         raise HTTPException(status_code=404, detail="Teacher not found")
     return teacher  
 
-@app.put("/teachers/{teacher_id}", tags=["CRUD"], response_model=TeacherResponse)
+@app.put("/teachers/{teacher_id}/", tags=["CRUD"], response_model=TeacherResponse)
 def update_teacher(teacher_id: int, teacher: TeacherCreate, db: dbDep):
     teacher_db = db.get(Teacher, teacher_id)
     if not teacher_db:
@@ -478,7 +478,7 @@ def update_teacher(teacher_id: int, teacher: TeacherCreate, db: dbDep):
     db.refresh(teacher_db)
     return teacher_db
 
-@app.delete("/teachers/{teacher_id}", tags=["CRUD"])
+@app.delete("/teachers/{teacher_id}/", tags=["CRUD"])
 def delete_teacher(teacher_id: int, db: dbDep):
     teacher = db.get(Teacher, teacher_id)
     if not teacher:
@@ -496,7 +496,7 @@ class LessonCreate(BaseModel):
 class LessonResponse(LessonCreate):
     id: int
 
-@app.post("/lessons", tags=["CRUD"], response_model=LessonResponse)
+@app.post("/lessons/", tags=["CRUD"], response_model=LessonResponse)
 def create_lesson(lesson: LessonCreate, db: dbDep):
     lesson_db = Lesson.model_validate(lesson)
     db.add(lesson_db)
@@ -504,18 +504,18 @@ def create_lesson(lesson: LessonCreate, db: dbDep):
     db.refresh(lesson_db)
     return lesson_db
 
-@app.get("/lessons", tags=["CRUD"], response_model=List[LessonResponse])
+@app.get("/lessons/", tags=["CRUD"], response_model=List[LessonResponse])
 def read_lessons(db: dbDep):
     return db.query(Lesson).all()
 
-@app.get("/lessons/{lesson_id}", tags=["CRUD"])
+@app.get("/lessons/{lesson_id}/", tags=["CRUD"])
 def read_lesson(lesson_id: int, db: dbDep):
     lesson = db.get(Lesson, lesson_id)
     if not lesson:
         raise HTTPException(status_code=404, detail="Lesson not found")
     return lesson  
 
-@app.put("/lessons/{lesson_id}", tags=["CRUD"], response_model=LessonResponse)
+@app.put("/lessons/{lesson_id}/", tags=["CRUD"], response_model=LessonResponse)
 def update_lesson(lesson_id: int, lesson: LessonCreate, db: dbDep):
     lesson_db = db.get(Lesson, lesson_id)
     if not lesson_db:
@@ -527,7 +527,7 @@ def update_lesson(lesson_id: int, lesson: LessonCreate, db: dbDep):
     db.refresh(lesson_db)
     return lesson_db
 
-@app.delete("/lessons/{lesson_id}", tags=["CRUD"])
+@app.delete("/lessons/{lesson_id}/", tags=["CRUD"])
 def delete_lesson(lesson_id: int, db: dbDep):
     lesson = db.get(Lesson, lesson_id)
     if not lesson:
@@ -576,7 +576,7 @@ class TeacherInfoResponse(BaseModel):
     degree: str
     position: str
 
-@app.get("/students/avg-grades", tags=["Queries"], response_model=List[StudentAvgGrade])
+@app.get("/students/avg-grades//", tags=["Queries"], response_model=List[StudentAvgGrade])
 def get_students_avg_grades(db: dbDep):
     result = db.query(
         Student.name.label("student_name"),
@@ -589,7 +589,7 @@ def get_students_avg_grades(db: dbDep):
 
     return result
 
-@app.get("/schedule/{group_id}", tags=["Queries"], response_model=List[ScheduleResponse])
+@app.get("/schedule/{group_id}/", tags=["Queries"], response_model=List[ScheduleResponse])
 def schedule(group_id: int, db: dbDep):
     result = db.query(
         Lesson.time.label("lesson_time"),
@@ -608,7 +608,7 @@ def schedule(group_id: int, db: dbDep):
 
     return result
 
-@app.get("/teacher-load", tags=["Queries"], response_model=List[TeacherCourseResponse])
+@app.get("/teacher-load/", tags=["Queries"], response_model=List[TeacherCourseResponse])
 def teacher_load(db: dbDep):
     result = db.query(
         Teacher.name.label("teacher"),
@@ -621,7 +621,7 @@ def teacher_load(db: dbDep):
 
     return result
 
-@app.get("/curator-load", tags=["Queries"], response_model=List[CuratorStudentsResponse])
+@app.get("/curator-load/", tags=["Queries"], response_model=List[CuratorStudentsResponse])
 def curator_load(db: dbDep):
     result = db.query(
         Curator.name.label("curator"),
@@ -637,7 +637,7 @@ def curator_load(db: dbDep):
 
     return result
 
-@app.get("/teacher-info", tags=["Queries"], response_model=List[TeacherInfoResponse])
+@app.get("/teacher-info/", tags=["Queries"], response_model=List[TeacherInfoResponse])
 def teacher_load(db: dbDep):
     result = db.query(
         Degree.title.label("degree"),
